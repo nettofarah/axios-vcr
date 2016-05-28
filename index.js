a235577ba4f87429848b17de40a850242cab04e9
Netto Farah <nettofarah@gmail.com>
2016-05-27 23:48:31 -0700

    Basic Request Middleware implementation

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ var ResponseMiddleware = require('./lib/ResponseMiddleware');
 function useCassete(cassetePath, cb) {
   var axios = require('axios');
 
-  var interceptor = axios.interceptors.response.use(
+  var responseInterceptor = axios.interceptors.response.use(
     ResponseMiddleware.success(cassetePath),
     ResponseMiddleware.failure
   );
 
+  var requestInterceptor = axios.interceptors.request.use(
+    RequestMiddleware.success(cassetePath),
+    RequestMiddleware.failure
+  )
+
   cb();
 
-  axios.interceptors.response.eject(interceptor);
+  axios.interceptors.response.eject(responseInterceptor);
+  axios.interceptors.request.eject(requestInterceptor);
 }
 
 module.exports = {
--- test/axios_vcr_test.js
+++ test/axios_vcr_test.js
@@ -7,6 +7,14 @@ function clearFixtures() {
   rimraf.sync('./test/fixtures');
 }
 
+function fileExists(path) {
+  try {
+    return fs.statSync(path).isFile();
+  } catch(e) {
+    return false
+  }
+}
+
 describe('Axios VCR', function() {
   this.timeout(10000);
   var cats = 'https://reddit.com/r/cats.json';
@@ -22,7 +30,7 @@ describe('Axios VCR', function() {
           var contents = JSON.parse(fs.readFileSync('./test/fixtures/cats.json'));
           assert.deepEqual(contents.data, response.data);
           done();
-        }).catch(function(err) { console.log(err); done(); });
+        });
       });
     });
 
@@ -48,8 +56,43 @@ describe('Axios VCR', function() {
           assert.equal(contents.statusText, response.statusText);
 
           done();
-        }).catch(function(err) { console.log(err); done(); });
+        });
       });
     });
+  });
+
+  describe('replaying', function() {
+    /*
+      This is a tricky test.
+      I'm not aware of any way to check that a network request has been made.
+      So instead we hit an unexisting URL that is backed by a cassete. We can now
+      check that the response is the same as the cassete file.
+    */
+    it('skips remote calls', function(done) {
+      var path = './test/static_fixtures/cats.json';
+      assert(fileExists(path));
+
+      VCR.useCassete(path, function () {
+        axios.get('http://something.com/unexisting').then(function(response) {
+          var contents = JSON.parse(fs.readFileSync(path));
+          assert.deepEqual(response.data, contents.data);
+          done();
+        });
+      });
+    });
+
+    it('makes remote call when a cassete is not available', function(done) {
+      var path = './test/static_fixtures/no_cats.json';
+
+      fs.unlinkSync(path);
+      assert(!fileExists(path));
+
+      VCR.useCassete(path, function () {
+        axios.get(cats).then(function(response) {
+          assert.equal(200, response.status);
+          done();
+        });
+      });
+    })
   });
 });
