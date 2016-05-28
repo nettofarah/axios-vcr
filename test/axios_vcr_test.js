6a0ee0ca129b702663d655abd9cda486f8024040
Netto Farah <nettofarah@gmail.com>
2016-05-27 23:05:00 -0700

    Basic Response Middleware implementation

---
+++ test/axios_vcr_test.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+var rimraf = require('rimraf');
+var assert = require('assert');
+var VCR = require('../index');
+
+function clearFixtures() {
+  rimraf.sync('./test/fixtures');
+}
+
+describe('Axios VCR', function() {
+  this.timeout(10000);
+  var cats = 'https://reddit.com/r/cats.json';
+  var axios = require('axios');
+
+  describe('recording', function() {
+    beforeEach(clearFixtures);
+    afterEach(clearFixtures);
+
+    it('generates stubs for requests', function(done) {
+      VCR.useCassete('./test/fixtures/cats.json', function () {
+        axios.get(cats).then(function(response) {
+          var contents = JSON.parse(fs.readFileSync('./test/fixtures/cats.json'));
+          assert.deepEqual(contents.data, response.data);
+          done();
+        }).catch(function(err) { console.log(err) });
+      });
+    });
+
+    it('works with nested folders', function(done) {
+      var cassetePath = './test/fixtures/reddit/r/cats.json';
+      VCR.useCassete(cassetePath, function () {
+        axios.get(cats).then(function(response) {
+          var contents = JSON.parse(fs.readFileSync(cassetePath));
+          assert.deepEqual(contents.data, response.data);
+          done();
+        }).catch(function(err) { console.log(err) });
+      });
+    });
+
+    it('stores headers', function(done) {
+      var cassetePath = './test/fixtures/cats.json';
+      VCR.useCassete(cassetePath, function () {
+        axios.get(cats).then(function(response) {
+          var contents = JSON.parse(fs.readFileSync(cassetePath));
+          assert.deepEqual(contents.headers, response.headers);
+          done();
+        }).catch(function(err) { console.log('fuu', err) });
+      });
+    });
+  });
+});
