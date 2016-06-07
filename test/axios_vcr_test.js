156131ce61fb7617eb6208a362291be73dc54e78
nettofarah <nettofarah@gmail.com>
2016-06-07 13:28:10 +0900

    remove semicolons

--- test/axios_vcr_test.js
+++ test/axios_vcr_test.js
@@ -1,65 +1,65 @@
-var fs = require('fs');
-var rimraf = require('rimraf');
-var assert = require('assert');
-var VCR = require('../index');
+var fs = require('fs')
+var rimraf = require('rimraf')
+var assert = require('assert')
+var VCR = require('../index')
 
 function clearFixtures() {
-  rimraf.sync('./test/fixtures');
+  rimraf.sync('./test/fixtures')
 }
 
 function fileExists(path) {
   try {
-    return fs.statSync(path).isFile();
+    return fs.statSync(path).isFile()
   } catch(e) {
     return false
   }
 }
 
 describe('Axios VCR', function() {
-  this.timeout(10000);
-  var cats = 'https://reddit.com/r/cats.json';
-  var axios = require('axios');
+  this.timeout(10000)
+  var cats = 'https://reddit.com/r/cats.json'
+  var axios = require('axios')
 
   describe('recording', function() {
-    beforeEach(clearFixtures);
-    afterEach(clearFixtures);
+    beforeEach(clearFixtures)
+    afterEach(clearFixtures)
 
     it('generates stubs for requests', function(done) {
       VCR.useCassette('./test/fixtures/cats.json', function () {
         axios.get(cats).then(function(response) {
-          var contents = JSON.parse(fs.readFileSync('./test/fixtures/cats.json'));
-          assert.deepEqual(contents.data, response.data);
-          done();
-        });
-      });
-    });
+          var contents = JSON.parse(fs.readFileSync('./test/fixtures/cats.json'))
+          assert.deepEqual(contents.data, response.data)
+          done()
+        })
+      })
+    })
 
     it('works with nested folders', function(done) {
-      var cassettePath = './test/fixtures/reddit/r/cats.json';
+      var cassettePath = './test/fixtures/reddit/r/cats.json'
       VCR.useCassette(cassettePath, function () {
         axios.get(cats).then(function(response) {
-          var contents = JSON.parse(fs.readFileSync(cassettePath));
-          assert.deepEqual(contents.data, response.data);
-          done();
-        }).catch(function(err) { console.log(err); done(); });
-      });
-    });
+          var contents = JSON.parse(fs.readFileSync(cassettePath))
+          assert.deepEqual(contents.data, response.data)
+          done()
+        }).catch(function(err) { console.log(err); done() })
+      })
+    })
 
     it('stores headers and status', function(done) {
-      var cassettePath = './test/fixtures/cats.json';
+      var cassettePath = './test/fixtures/cats.json'
       VCR.useCassette(cassettePath, function () {
         axios.get(cats).then(function(response) {
-          var contents = JSON.parse(fs.readFileSync(cassettePath));
+          var contents = JSON.parse(fs.readFileSync(cassettePath))
 
-          assert.deepEqual(contents.headers, response.headers);
-          assert.equal(contents.status, response.status);
-          assert.equal(contents.statusText, response.statusText);
+          assert.deepEqual(contents.headers, response.headers)
+          assert.equal(contents.status, response.status)
+          assert.equal(contents.statusText, response.statusText)
 
-          done();
-        });
-      });
-    });
-  });
+          done()
+        })
+      })
+    })
+  })
 
   describe('replaying', function() {
     /*
@@ -69,33 +69,33 @@ describe('Axios VCR', function() {
       check that the response is the same as the cassette file.
     */
     it('skips remote calls', function(done) {
-      var path = './test/static_fixtures/cats.json';
-      assert(fileExists(path));
+      var path = './test/static_fixtures/cats.json'
+      assert(fileExists(path))
 
       VCR.useCassette(path, function () {
         axios.get('http://something.com/unexisting').then(function(response) {
-          var contents = JSON.parse(fs.readFileSync(path));
-          assert.deepEqual(response.data, contents.data);
-          done();
-        });
-      });
-    });
+          var contents = JSON.parse(fs.readFileSync(path))
+          assert.deepEqual(response.data, contents.data)
+          done()
+        })
+      })
+    })
 
     it('makes remote call when a cassette is not available', function(done) {
-      var path = './test/static_fixtures/no_cats.json';
+      var path = './test/static_fixtures/no_cats.json'
 
       try {
-        fs.unlinkSync(path);
+        fs.unlinkSync(path)
       } catch(e) {}
 
-      assert(!fileExists(path));
+      assert(!fileExists(path))
 
       VCR.useCassette(path, function () {
         axios.get(cats).then(function(response) {
-          assert.equal(200, response.status);
-          done();
-        });
-      });
+          assert.equal(200, response.status)
+          done()
+        })
+      })
     })
-  });
-});
+  })
+})
